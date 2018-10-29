5028c2cf3f35bd20d79032e4e227ac8ca6d96a9f
Collin Brill <collin.brill@gmail.com>
2018-10-29 13:00:28 -0700

    turn-in quality

--- src/hooks.c
+++ src/hooks.c
@@ -86,6 +86,5 @@ int	key_press(int key, t_params *e)
 			(t_palette){rgb(247, 62, 103), 1, 4, 2, 0, 255, 0},
 			(t_palette){rgb(113, 188, 167), 1, 3, 5, 255, 255, 255}
 		}[(key > 90) ? key - 83 : key - 82];
-		;
 	return (0);
 }
